c39da430198693b5f4e5e9f0f7f0d73dda3e3baf
galactusclb <chanakabalasuriya123@gmail.com>
2021-07-09 18:37:49 +0530

    v1.0.2"

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# Tast_restaurant_clone" 
+"# Tast_restaurant_clone"
+
+UI inspiration from
+https://dribbble.com/shots/12998351-Food-Mobile-APP-Landing-UX-UI-Design?showSimilarShots=true&_=1625668184028#
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // add nav background colors after 30px
 window.addEventListener("scroll", () => {
-	console.log("ggg");
 	const navi = document.getElementById("navbar-main");
 	if (window.pageYOffset >= 30 && !navi.classList.contains("addBackground")) {
 		navi.classList.add("addBackground");
@@ -12,3 +11,21 @@ window.addEventListener("scroll", () => {
 		navi.classList.remove("addBackground");
 	}
 });
+
+// mouse over parallax effcet
+let navbar = document.getElementById("navbar-main");
+let hero = document.getElementById("hero");
+navbar.addEventListener("mousemove", parallax);
+hero.addEventListener("mousemove", parallax);
+
+function parallax(e) {
+	hero.querySelectorAll(".layer").forEach((layer) => {
+		const speed = layer.getAttribute("data-speed");
+		const rotate = layer.getAttribute("data-rotate");
+
+		const x = (window.innerWidth - e.pageX * speed) / 100;
+		const y = (window.innerHeight - e.pageY * speed) / 100;
+
+		layer.style.transform = `translateX(${x}px) translateY(${y}px) rotate(${rotate}deg)`;
+	});
+}
